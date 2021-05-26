4cb570b44175f79bec0c25b1f708a4151b218fcc
Lais-Peixoto <laispeixotodemoraes@gmail.com>
2021-05-26 19:48:17 -0300

    Banco de dados acrescentado como novo serviço à aplicação através do método AddDbContext passando a classe ApplicationContext e uma opção de configuração (obtida pelo objeto Configuration e o método GetConnectionString).

--- CasaDoCodigo/Startup.cs
+++ CasaDoCodigo/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -32,6 +33,12 @@ namespace CasaDoCodigo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options => 
+                options.UseSqlServer(connectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
