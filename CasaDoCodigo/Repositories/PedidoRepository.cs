5f48607e541a1afd1cc7daf9beb43b68563b8802
Lais-Peixoto <laispeixotodemoraes@gmail.com>
2021-05-29 13:13:12 -0300

    Criação de métodos para obter e gravar o id do pedido na sessão.

--- CasaDoCodigo/Repositories/PedidoRepository.cs
+++ CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,23 @@ namespace CasaDoCodigo.Repositories
 {
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
-        public PedidoRepository(ApplicationContext contexto) : base(contexto)
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public PedidoRepository(ApplicationContext contexto, IHttpContextAccessor contextAccessor) : base(contexto)
         {
+            _contextAccessor = contextAccessor;
+        }
+
+        // método para obter o id do pedido que será armazenado na sessão
+        private int? GetPedidoId()
+        {
+           return _contextAccessor.HttpContext.Session.GetInt32("pedidoId");
+        }
+
+        // método para gravar o pedidoId na sessão
+        private void SetPedidoId(int pedidoId)
+        {
+            _contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
         }
     }
 }
