9eca97b03587594310667e59bebeac27a5f89cff
Lais-Peixoto <laispeixotodemoraes@gmail.com>
2021-05-29 15:31:53 -0300

    Correção de erro na classe Startup. O serviço HttpContextAccessor não é mais adicionado por padrão. Modificação da view de carrinho.

--- CasaDoCodigo/Startup.cs
+++ CasaDoCodigo/Startup.cs
@@ -49,6 +49,8 @@ namespace CasaDoCodigo
 
             services.AddDistributedMemoryCache();
             services.AddSession();
+
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
